e2f6823e467a125b19f496eaba99f9fca2ffa6f5
SOOD-11 <149188980+SOOD-11@users.noreply.github.com>
2026-06-16 19:34:31 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,59 +2,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Itemspage from "./api/Itemspage";
 
-// 1. Define Item type (match your DB model)
-type Item = {
-  id: number;
-  name: string;
-  createdAt: string;
-  updatedAt?: string;
-};
+
 
 const App = () => {
-  const [items, setItems] = useState<Item[]>([]);
-  const [name, setName] = useState<string>("");
-  const [date, setDate] = useState<string>("");
 
-
-
-  // 2. Get all items
-  const fetchAllItems = async (): Promise<void> => {
-    try {
-      const res = await axios.get<Item[]>(`${import.meta.env.VITE_BASE_URL}`);
-      setItems(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // 3. Search by name
-  const searchByName = async (): Promise<void> => {
-    try {
-      const res = await axios.get<Item[]>(`${import.meta.env.VITE_BASE_URL}/search`, {
-        params: { name },
-      });
-      setItems(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // 4. Filter by date
-  const filterByDate = async (): Promise<void> => {
-    try {
-      const res = await axios.get<Item[]>(`${import.meta.env.VITE_BASE_URL}/by-date`, {
-        params: { date },
-      });
-      setItems(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // initial load
-  useEffect(() => {
-    fetchAllItems();
-  }, []);
 
   return (
     <div style={{ padding: "20px" }}>
